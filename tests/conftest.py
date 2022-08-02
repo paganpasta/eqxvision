70975056adf157966ede29f49d91792b7fc55c5c
paganpasta <aditya.91.singh@gmail.com>
2022-08-03 00:39:44 +0100

    added static module for testing preds

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,9 @@ import random
 import jax.numpy as jnp
 import jax.random as jrandom
 import pytest
+import torch
+from PIL import Image
+from torchvision.transforms import transforms
 
 
 @pytest.fixture()
@@ -15,5 +18,21 @@ def getkey():
 
 
 @pytest.fixture()
-def random_image(input_shape):
-    return jrandom.uniform(key=jrandom.PRNGKey(0), shape=input_shape)
+def demo_image():
+    img = Image.open("static/img.png")
+    img = img.convert("RGB")
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
+    return transform(img).unsqueeze(0)
+
+
+@pytest.fixture()
+def alexnet_preds():
+    ckpt = torch.load("static/alexnet.pred.pth")
+    return ckpt["feats"].detach().numpy(), ckpt["output"].detach().numpy()
--- tests/test_weight_loading.py
+++ tests/test_weight_loading.py
@@ -1,10 +1,13 @@
+import equinox as eqx
+
 import eqxvision.models as models
 import eqxvision.utils as utils
 
 
-def test_load_weights(getkey):
+def test_load_weights(getkey, demo_image, alexnet_preds):
     model = models.alexnet(num_classes=1000)
     new_model = utils.load_torch_weights(
         model=model, url="https://download.pytorch.org/models/alexnet-owt-7be5be79.pth"
     )
+    new_model = eqx.tree_inference(new_model, True)
     assert model != new_model
